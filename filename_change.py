8542c6eba6a49986ad6ae7a7fc5bfd8b7d2622ec
marathonman4202 <roy.haggerty@oregonstate.edu>
2015-05-25 14:07:56 -0700

    renaming long list of pngs

--- filename_change.py
+++ filename_change.py
@@ -1,12 +1,23 @@
 import os
-path = "C:\\Users\\haggertr\\Desktop\\Documents\\work - OSU\\research\\WW2100\\Research\\snow\\snotel data\\"
+path = "C:\\code\\maplot pngs\\anim\\"
 #path = "C:\\Users\\haggertr\\Desktop\\Documents\\work - OSU\\research\\WW2100\\Research\\results2\\pngs\\"
 #path = "C:\\Users\\haggertr\\Desktop\\Documents\\work - OSU\\research\\WW2100\\Research\\results2\\altFiles\\Historic_Ref_Apr20_v9823\\"
 for filename in os.listdir(path):
-    if ".zip" not in filename:
-        print filename
+    filename2 = filename[:5]+filename[6:-4]+filename[5]+'.png'
+    if filename2[5].isdigit(): filename2 = filename2[:5]+filename2[6:-4]+filename2[5]+'.png'
+    else: filename2 = filename2[:5]+filename2[5:-5]+'0'+filename2[-5:]
+    if filename2[5].isdigit(): filename2 = filename2[:5]+filename2[6:-4]+filename2[5]+'.png'
+    else: filename2 = filename2[:5]+filename2[5:-6]+'0'+filename2[-6:]
+    filename3 ='img'
+    for letter in filename2:
+        if letter.isdigit(): filename3 = filename3+letter
+    filename3 = filename3+'.png'
+    print filename3
+    os.rename(path+filename, path+filename3)
+#        print filename
 #        os.rename(path+filename, path+filename[:-4]+"w EXTREME.png")
-        os.rename(path+filename, path+filename[:-4]+".csv")
+#        os.rename(path+filename, path+filename[:-4]+".csv")
+#        os.rename(path+filename, path+filename[:-4]+".csv")
 #    if "_Ref_" in filename:
 #        filename_new = filename.replace("_Ref_", "_Historic_")
 #        print filename_new
