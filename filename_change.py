3f95c1d1f419aa00820858fc0cca7ea048852921
marathonman4202 <roy.haggerty@oregonstate.edu>
2015-05-26 20:27:05 -0700

    rename larger group of png files for movie (img)

--- filename_change.py
+++ filename_change.py
@@ -1,19 +1,22 @@
 import os
-path = "C:\\code\\maplot pngs\\anim\\"
+path = "C:\\code\\maplot pngs\\anim Aug\\"
+#path = "D:\\Roy junk after 5-26-15\\"
 #path = "C:\\Users\\haggertr\\Desktop\\Documents\\work - OSU\\research\\WW2100\\Research\\results2\\pngs\\"
 #path = "C:\\Users\\haggertr\\Desktop\\Documents\\work - OSU\\research\\WW2100\\Research\\results2\\altFiles\\Historic_Ref_Apr20_v9823\\"
 for filename in os.listdir(path):
-    filename2 = filename[:5]+filename[6:-4]+filename[5]+'.png'
-    if filename2[5].isdigit(): filename2 = filename2[:5]+filename2[6:-4]+filename2[5]+'.png'
-    else: filename2 = filename2[:5]+filename2[5:-5]+'0'+filename2[-5:]
-    if filename2[5].isdigit(): filename2 = filename2[:5]+filename2[6:-4]+filename2[5]+'.png'
-    else: filename2 = filename2[:5]+filename2[5:-6]+'0'+filename2[-6:]
-    filename3 ='img'
-    for letter in filename2:
-        if letter.isdigit(): filename3 = filename3+letter
-    filename3 = filename3+'.png'
-    print filename3
-    os.rename(path+filename, path+filename3)
+    if filename[0] == 'Q':
+        print filename
+        filename2 = filename[:5]+filename[6:-4]+filename[5]+'.png'
+        if filename2[5].isdigit(): filename2 = filename2[:5]+filename2[6:-4]+filename2[5]+'.png'
+        else: filename2 = filename2[:5]+filename2[5:-5]+'0'+filename2[-5:]
+        if filename2[5].isdigit(): filename2 = filename2[:5]+filename2[6:-4]+filename2[5]+'.png'
+        else: filename2 = filename2[:5]+filename2[5:-6]+'0'+filename2[-6:]
+        filename3 ='img'
+        for letter in filename2:
+            if letter.isdigit(): filename3 = filename3+letter
+        filename3 = filename3+'.png'
+        print filename3
+        os.rename(path+filename, path+filename3)
 #        print filename
 #        os.rename(path+filename, path+filename[:-4]+"w EXTREME.png")
 #        os.rename(path+filename, path+filename[:-4]+".csv")
