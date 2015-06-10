ecceae218fcce5e243f902cacd198ddae101e949
marathanman4202 <roy.haggerty@oregonstate.edu>
2015-06-09 17:32:58 -0700

    script to split a multi-column csv into several

---
+++ split_to_multiple_csv.py
@@ -0,0 +1,18 @@
+
+import pandas as pd
+#directory_path = "C:\\Users\\haggertr\\Desktop\\Roy\\Research\\WW2100\\Research\\results2\\Files\\"
+directory_path = "C:\\Users\\haggertr\\Desktop\\test\\"
+scenarios_list = ['HighClim']
+basic_name = 'HBV_Flows_(m3_s)_Ref_Run0.csv'
+
+for scenario in scenarios_list:
+    file_model_csv_w_path = directory_path + basic_name.replace('Ref', scenario)
+    df = pd.read_csv(file_model_csv_w_path)
+    new_file_list = list(df.columns.values)
+    for new_file in new_file_list[1:9]:
+        new_file2 = new_file.replace('/','_')
+        new_file_name = directory_path + new_file2[1:].replace(' ','_') + '.csv'
+        columns = [new_file_list[0],new_file]
+        df.to_csv(new_file_name,header=columns)
+        print new_file_name
+assert False
